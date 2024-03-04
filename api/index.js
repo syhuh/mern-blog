372c0dbd87b8aeb3e83e736f856ad6d0bc6d86bc
syhuh <syhuh1125@gmail.com>
2024-03-04 21:15:54 +0900

    add create a post api route and apply mongoose unique validator

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
+import postRoute from "./routes/post.route.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -30,6 +31,7 @@ app.get("/test", (req, res) => {
 
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/post", postRoute);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
---
+++ api/models/post.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import uniqueValidator from "mongoose-unique-validator";
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      default:
+        "https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2021/09/how-to-write-a-blog-post.png",
+    },
+    categories: {
+      type: String,
+      default: "uncategorized",
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+postSchema.plugin(uniqueValidator, { message: "{VALUE} is already taken." });
+const Post = mongoose.model("Post", postSchema);
+export default Post;
