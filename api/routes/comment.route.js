cafedef2b84f52f26282e644425af810ad6735a3
syhuh <syhuh1125@gmail.com>
2024-03-15 22:32:42 +0900

    add like functionality to the comment component

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -3,6 +3,7 @@ import { verifyToken } from "../utils/verifyUser.js";
 import {
   createComment,
   getPostComments,
+  likeComment,
 } from "../controllers/comment.controller.js";
 import { get } from "mongoose";
 
@@ -10,5 +11,6 @@ const router = express.Router();
 
 router.post("/create", verifyToken, createComment);
 router.get("/getPostComments/:postId", getPostComments);
+router.put("/likeComment/:commentId", verifyToken, likeComment);
 
 export default router;
--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
 import moment from "moment";
+import { FaThumbsUp } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
-export default function Comment({ comment }) {
+export default function Comment({ comment, onLike }) {
   const [user, setUser] = useState(null);
-  console.log(user);
+  const { currentUser } = useSelector((state) => state.user);
+
   useEffect(() => {
     const getUser = async () => {
       try {
@@ -37,6 +40,26 @@ export default function Comment({ comment }) {
           </span>
         </div>
         <p className="text-gray-500 pb-2">{comment.content}</p>
+        <div className="flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2">
+          <button
+            type="button"
+            onClick={() => onLike(comment._id)}
+            className={`text-gray-400 hover:text-blue-500 
+            ${
+              currentUser &&
+              comment.likes.includes(currentUser._id) &&
+              "!text-blue-500 hover:!text-gray-400"
+            }`}
+          >
+            <FaThumbsUp />
+          </button>
+          <p className="text-gray-400">
+            {comment.numberOfLikes > 0 &&
+              comment.numberOfLikes +
+                " " +
+                (comment.numberOfLikes === 1 ? "like" : "likes")}
+          </p>
+        </div>
       </div>
     </div>
   );
