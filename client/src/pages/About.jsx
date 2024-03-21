5a8c78ea990d370f10a7b9f184bbea90f5fbc558
syhuh <syhuh1125@gmail.com>
2024-03-21 20:15:31 +0900

    complete about and projects page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,30 @@
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="min-h-screen flex items-center">
+      <div className="flex flex-col max-w-2xl mx-auto p-3 ">
+        <h1 className="text-3xl font-semibold text-center my-7">
+          Paxkhan의 블로그
+        </h1>
+        <div className="flex flex-col gap-6 text-gray-500 text-md">
+          <p>
+            팍스칸의 블로그에 오신 것을 환영합니다! 팍스칸은 열정적인 개발자이며
+            임베디드, 윈도우즈, 공장자동화, 웹, AI에 걸쳐 다양한 이력을 가지고
+            있습니다.
+          </p>
+          <p>
+            이 블로그는 팍스칸의 개발 이야기와 경험을 공유하고자 만들어졌습니다.
+            팍스칸은 여러분들과 함께 자신의 경험을 나누고, 함께 성장하고자
+            합니다. 블로그 포스팅에 대한 피드백은 언제나 환영합니다. 다른 분의
+            댓글에 좋아요를 누르거나 대댓글을 다는 것으로도 피드백을 줄 수
+            있습니다.
+          </p>
+          <p>
+            이 블로그는 Udemy Dr. Sahand Ghavidel 님의 강의를 기반으로
+            만들었습니다. 계속해서 기능을 보강하여 더 나은 블로그를
+            만들어보겠습니다. 감사합니다.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,3 +1,14 @@
+import CallToAction from "../components/CallToAction";
+
 export default function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div className="max-w-2xl min-h-screen flex flex-col justify-center items-center mx-auto gap-6 p-3">
+      <h1 className="text-3xl font-semibold text-center my-7">Projects</h1>
+      <p className="text-md text-gray-500">
+        HTML, CSS, JavaScript를 배우면서 재미있고 매력적인 프로젝트를
+        만들어보자!
+      </p>
+      <CallToAction />
+    </div>
+  );
 }
